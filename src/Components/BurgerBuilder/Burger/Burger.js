529cf948de6900d43f8eadf94b5308d7c3d29830
shahed <shahed.rahman88@gmail.com>
2022-03-29 15:59:03 +0600

    ingredient images added conditionaly

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Ingredient from "../Ingredient/Ingredient";
+
 const Burger = props => {
     return (
         <div>
-            <p>Burger</p>
+            <Ingredient type="bread-top" />
+            <Ingredient type="cheese" />
+            <Ingredient type="salad" />
+            <Ingredient type="meat" />
+            <Ingredient type="bread-bottom" />
         </div>
     )
 }
--- src/Components/BurgerBuilder/Ingredient/Ingredient.js
+++ src/Components/BurgerBuilder/Ingredient/Ingredient.js
@@ -1,7 +1,36 @@
-const Ingredient = () => {
+import BreadTop from '../../../assets/images/top.png';
+import BreadBottom from '../../../assets/images/bottom.png';
+import Meat from '../../../assets/images/meat.png';
+import Salad from '../../../assets/images/salad.png';
+import Cheese from '../../../assets/images/cheese.png';
+
+
+const Ingredient = props => {
+    let ingredient = null;
+
+    switch(props.type) {
+        case 'bread-bottom':
+            ingredient = <div><img src={BreadBottom} alt="Bottom bread" /></div>;
+                break;
+        case 'bread-top':
+            ingredient = <div><img src={BreadTop} alt="Bottom top" /></div>;
+                break;
+        case 'meat':
+            ingredient = <div><img src={Meat} alt="Meat" /></div>;
+                break;
+        case 'salad':
+            ingredient = <div><img src={Salad} alt="Salad" /></div>;
+                break;
+        case 'cheese':
+            ingredient = <div><img src={Cheese} alt="Cheese" /></div>;
+                break;
+        default:
+            ingredient = null;
+    }
+
     return (
         <div>
-
+            {ingredient}
         </div>
     )
 }
