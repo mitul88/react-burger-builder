1e9557eb7494f9db28beeab7b16081e47bd7f6be
shahed <shahed.rahman88@gmail.com>
2022-03-29 14:53:58 +0600

    css class added to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,17 @@
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import "./Header.css";
 
 const Header = () => {
     return (
-        <div>
+        <div className="Navigation">
             <Navbar style={{
                     backgroundColor: "#d70f64",
                     height: "70px"
                 }}>
-                <NavbarBrand href='/' className="mr-auto ml-md-5"> Brand </NavbarBrand>
+                <NavbarBrand href='/' className="mr-auto ml-md-5 Brand"> Brand </NavbarBrand>
                 <Nav>
                     <NavItem className="mr-md-5">
-                        <NavLink href="#">
+                        <NavLink href="#" className="NavLink">
                             Menu
                         </NavLink>
                     </NavItem>
