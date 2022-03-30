81a297aaae4e2c7d3d333b9ef49d501654b261fb
shahed <shahed.rahman88@gmail.com>
2022-03-30 15:40:16 +0600

    controls componenet created

---
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -0,0 +1,24 @@
+import { Card, CardBody, CardFooter, CardHeader, Button } from "reactstrap";
+
+
+const Controls = () => {
+    return (
+        <div>
+            <Card>
+                <CardHeader>
+                    <h4>Add Ingredients</h4>
+                </CardHeader>
+                <CardBody>
+
+                </CardBody>
+                <CardFooter>
+                    <h5>
+                        Price: BDT
+                    </h5>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
+
+export default Controls;
