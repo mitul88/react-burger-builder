f8f0ea4a58765262a9929d508a6de8f3fe05e3b4
shahed <shahed.rahman88@gmail.com>
2022-04-19 00:41:03 +0600

    converted checkout to class component

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,9 +1,13 @@
-const Checkout = () => {
-    return (
-        <div>
-            Checkout
-        </div>
-    )
+import React, { Component } from "react";
+
+export class Checkout extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
 
 export default Checkout;
