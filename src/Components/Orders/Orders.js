19a6794e1545859865e503ed053034d0e0120f4a
shahed <shahed.rahman88@gmail.com>
2022-04-21 17:39:48 +0600

    mapDispatchToProps added

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchOrders } from "../../redux/actionCreators";
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchOrders: () => dispatch(fetchOrders()),
+    }
+}
 
 class Orders extends Component {
     render() {
