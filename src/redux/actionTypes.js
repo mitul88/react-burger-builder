0dc806b45bec0adb3d6bee97a35c176ff0c5a081
shahed <shahed.rahman88@gmail.com>
2022-04-22 17:43:49 +0600

    auth load and logout action type added

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -8,3 +8,5 @@ export const ORDER_LOAD_FAILED = "ORDER_LOAD_FAILED";
 
 export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_FAILED = "AUTH_FAILED";
+export const AUTH_LOADING = "AUTH_LOADING";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
