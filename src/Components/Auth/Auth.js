5bf38634936f78365211b777e536b48bc1962ab3
shahed <shahed.rahman88@gmail.com>
2022-04-22 13:50:48 +0600

    auth component created

---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Formik } from "formik";
+
+
+class Auth extends Component {
+    render() {
+        return (
+            <div>
+                Auth
+            </div>
+        )
+    }
+}
+
+
+export default Auth;
