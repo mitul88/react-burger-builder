3f2c9127cbc096db6bba3ca4943eb0a7bf6e44e2
shahed <shahed.rahman88@gmail.com>
2022-04-23 17:51:21 +0600

    logout connected to store, useNavigate used to redirect

--- src/Components/Auth/Logout.js
+++ src/Components/Auth/Logout.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import { useNavigate } from "react-router";
+
 import { connect } from "react-redux";
 
 
-class Logout extends Component {
 
+import { logout } from "../../redux/authActionCreators";
+
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: ()=> dispatch(logout()),
+    }
 }
+
+class Logout extends Component {
+    render() {
+        const navigate = useNavigate();
+
+        return navigate("./", { replace: true })
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Logout);
